87ef74f423cf4a8e397c06e4d34445a52f23dbe3
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-16 10:21:11 +0100

    input values

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 from matplotlib.axes import Axes  # IDE doesn't understand subplots return type.
 ax: Axes
 
+
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-fig, ax = plt.subplots() # The variable <<fig>> represents the entire figure or collection of plots that are generated
-ax.plot(squares, linewidth=3)
+plt.style.use('seaborn')
+fig, ax = plt.subplots()  # The variable <<fig>> represents the entire figure or collection of plots that are generated
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
