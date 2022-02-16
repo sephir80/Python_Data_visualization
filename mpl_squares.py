2423491e14269df9b686dfa42d3d6666a4df6d2a
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-16 10:40:30 +0100

    punti colorati

--- mpl_squares.py
+++ mpl_squares.py
@@ -6,13 +6,13 @@ ax: Axes
 x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]   # [x**2] -> [x^2]
 
-#input_values = [1, 2, 3, 4, 5]
-#squares = [1, 4, 9, 16, 25]
+# input_values = [1, 2, 3, 4, 5]
+# squares = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()  # The variable <<fig>> represents the entire figure or collection of plots that are generated
-#ax.plot(x_values, y_values, linewidth=3)  # Disegna una linea che passa per i punti
-ax.scatter(x_values, y_values, s=10)     # Disegna un punto di size=s
+# ax.plot(x_values, y_values, linewidth=3)  # Disegna una linea che passa per i punti
+ax.scatter(x_values, y_values, c='red', s=10)     # Disegna un punto di size=s e di colore c(rosso)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
