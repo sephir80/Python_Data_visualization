9b39546244fe747404cd38fdbe61921177415bc3
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-16 10:38:04 +0100

    generare una serie di numeri e calcolare i quadrati in un altra tabella

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,18 +3,24 @@ from matplotlib.axes import Axes  # IDE doesn't understand subplots return type.
 ax: Axes
 
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]   # [x**2] -> [x^2]
+
+#input_values = [1, 2, 3, 4, 5]
+#squares = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()  # The variable <<fig>> represents the entire figure or collection of plots that are generated
-ax.plot(input_values, squares, linewidth=3)  # Disegna una linea che passa per i punti
-ax.scatter(input_values, squares, s=100)     # Disegna un punto di size=s
+#ax.plot(x_values, y_values, linewidth=3)  # Disegna una linea che passa per i punti
+ax.scatter(x_values, y_values, s=10)     # Disegna un punto di size=s
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
 
 # Set size of tick labels
 ax.tick_params(axis='both', labelsize=8)
